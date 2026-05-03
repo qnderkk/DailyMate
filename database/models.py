58e2102df2423936bb60ce0d3de999abddf492a7
qnderkk <Qndi@yandex.ru>
2026-05-03 09:02:01 +0300

    add some models

--- database/models.py
+++ database/models.py
@@ -1,13 +1,21 @@
 from datetime import datetime
+import enum
 
-from sqlalchemy import BigInteger, String, DateTime
+from sqlalchemy import Integer, BigInteger, String, DateTime, ForeignKey, Boolean, Enum, CheckConstraint, Text
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.sql import func
 
 
+class TaskStatus(enum.Enum):
+    pending = "pending"
+    completed = "completed"
+    overdue = "overdue"
+    postponed = "postponed"
+
+
 class Base(DeclarativeBase):
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
 
 
 class User(Base):
@@ -17,5 +25,47 @@ class User(Base):
     username: Mapped[str] = mapped_column(String(64), unique=True, nullable=False)
     settings: Mapped[dict] = mapped_column(JSONB)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    user_id: Mapped[int] = mapped_column(BigInteger, ForeignKey("users.id", ondelete="CASCADE"))
+    name: Mapped[str] = mapped_column(String(128), nullable=False)
+    color_hex: Mapped[str] = mapped_column(String(128), default="#FFFFFF")
+
+
+class Checklist(Base):
+    __tablename__ = "checklists"
+
+    user_id: Mapped[int] = mapped_column(BigInteger, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    title: Mapped[str] = mapped_column(String(128), nullable=False)
+    steps: Mapped[dict] = mapped_column(JSONB)
+    is_template: Mapped[bool] = mapped_column(Boolean)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    user_id: Mapped[int] = mapped_column(BigInteger, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    category_id: Mapped[int] = mapped_column(BigInteger, ForeignKey("categories.id", ondelete="CASCADE"))
+    title: Mapped[str] = mapped_column(String(128), nullable=False)
+    description: Mapped[str] = mapped_column(String(128))
+    due_data: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+    status: Mapped[TaskStatus] = mapped_column(Enum(TaskStatus))
+    priority: Mapped[int] = mapped_column(
+        Integer,
+        CheckConstraint("priority >= 1 AND priopity <= 3")
+    )
+    original_text: Mapped[str] = mapped_column(Text , nullable=False)
+    ai_extraction: Mapped[dict] = mapped_column(JSONB)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    completed_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+
+
+
+
+
 
 
